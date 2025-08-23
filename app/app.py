5a48383aa00e60f3b04e722b1dd3d29a9fbfa949
Maisara Sherif <maisara.sherif.ms@gmail.com>
2025-08-23 12:28:18 +0000

    update ci files

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ VERSION = os.environ.get('VERSION', '1.0.0')
 @app.route('/')
 def hello_world():
     return jsonify({
-        'message': 'GitOps CI/CD with RANCHER FLEET',
+        'message': 'Hello World from Flask CI/CD!',
         'hostname': socket.gethostname(),
         'timestamp': datetime.utcnow().isoformat(),
         'version': VERSION,
