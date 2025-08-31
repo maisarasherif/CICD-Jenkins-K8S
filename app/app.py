5346f40e0839807826ed3a93ac2b7bd05298b8f8
Maisara <maisara.sherif.ms@gmail.com>
2025-08-31 20:09:16 +0400

    update app

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ VERSION = os.environ.get('VERSION', '1.0.0')
 @app.route('/')
 def hello_world():
     return jsonify({
-        'message': 'My Canary CI/CD with ArgoCD!',
+        'message': 'My Canary V2.0 CI/CD with ArgoCD!',
         'hostname': socket.gethostname(),
         'timestamp': datetime.utcnow().isoformat(),
         'version': VERSION,
