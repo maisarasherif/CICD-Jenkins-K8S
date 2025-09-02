9beaca3ac71c2a8794652c6af99e8f784447f7f1
Maisara <maisara.sherif.ms@gmail.com>
2025-09-02 23:39:04 +0400

    updated app

--- app/app.py
+++ app/app.py
@@ -4,23 +4,23 @@ import socket
 from datetime import datetime
 
 app = Flask(__name__)
-
+'''
 BUILD_DATE = os.environ.get('BUILD_DATE', 'unknown')
 VCS_REF = os.environ.get('VCS_REF', 'unknown')
 VERSION = os.environ.get('VERSION', '1.0.0')
-
+'''
 @app.route('/')
 def hello_world():
     return jsonify({
-        'message': 'CI/CD with Jenkins and ArgoCD: Blue/Green strategy',
+        'message': '[MEGA Project] CI/CD with Jenkins and ArgoCD: Blue/Green strategy',
         #'hostname': socket.gethostname(),
-        'timestamp': datetime.utcnow().isoformat(),
-        'version': VERSION,
-        'build_date': BUILD_DATE,
-        'git_commit': VCS_REF[:7] if VCS_REF != 'unknown' else 'unknown',
-        'environment': os.environ.get('ENVIRONMENT', 'development')
+        #'timestamp': datetime.utcnow().isoformat(),
+        #'version': VERSION,
+        #'build_date': BUILD_DATE,
+        #'git_commit': VCS_REF[:7] if VCS_REF != 'unknown' else 'unknown',
+        #'environment': os.environ.get('ENVIRONMENT', 'development')
     })
-
+'''
 @app.route('/version')
 def version_info():
     return jsonify({
@@ -29,7 +29,7 @@ def version_info():
         'git_commit': VCS_REF[:7] if VCS_REF != 'unknown' else 'unknown',
         'hostname': socket.gethostname(),
         'environment': os.environ.get('ENVIRONMENT', 'development')
-    })
+    })'''
 
 @app.route('/health')
 def health_check():
